c8ac2dab3d8efb33ae2b1aefe86e598ceb4f7db5
ErickKayisis <alansegovian2@gmail.com>
2025-07-21 15:55:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("mostrarCarta").addEventListener("click", function() {
+    const carta = document.getElementById("carta");
+    carta.classList.remove("hidden");
+});
+
+document.getElementById("mostrarImagen").addEventListener("click", function() {
+    const imagen = document.getElementById("imagen");
+    imagen.classList.remove("hidden");
+});
